cd6559e408567addbc8a33023e5f6aa550498de6
MeThakur <menotthakur@gmail.com>
2024-08-30 08:53:19 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,89 +1,89 @@
-#include "Database.h"
-#include "Executor.h"
-#include "Parser.h"
-#include <arpa/inet.h>
-#include <cerrno>
-#include <cstdlib>
-#include <cstring>
-#include <exception>
-#include <functional>
 #include <iostream>
-#include <netdb.h>
+#include <cstdlib>
 #include <string>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <thread>
+#include <cstring>
 #include <unistd.h>
-#include <vector>
-#define BUFSIZE 1024
-
-void handleConnection(std::reference_wrapper<redis::Database> database,
-                      int client) {
-  char buf[BUFSIZE];
-  ssize_t n = 0;
-  while ((n = recv(client, buf, BUFSIZE, 0)) > 0) {
-    redis::Parser p;
-    std::string cmdText(buf, n);
-    try {
-      auto cmd = p.parse(cmdText);
-      
-      auto executor = redis::Executor{cmd, database, client};
-      executor.execute();
-    } catch (const std::exception &e) {
-      // send(client, e.what(), std::strlen(e.what()), 0);
-    }
-  }
-  close(client);
-}
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible
-  // when running tests.
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
   // Uncomment this block to pass the first stage
   //
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
-  }
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
+     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+     if (server_fd < 0) {
+      std::cerr << "Failed to create server socket\n";
+      return 1;
+     }
+  
+  // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
-      0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
+ 
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
-      0) {
+ 
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
+ 
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
+ 
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  std::vector<std::thread> threads;
-  redis::Database database;
+ 
+  std::cout << "Waiting for a client to connect...\n";
+ 
+  //accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len); //the return value of accept function stored in client_fd.holds the socket file descriptor of the newly accepted client connection.
+  //send(client_fd, "+PONG\r\n", 7, 0);   // "+PONG\r\n": This is the message to be sent to the client. It's a string containing the characters "+PONG\r\n". The \r\n at the end is a carriage return and line feed, which is a common way to terminate a message in network communication. 
+  send(client_fd, "+PONG\r\n", 7, 0);   // "+PONG\r\n": This is the message to be sent to the client. It's a string containing the characters "+PONG\r\n". The \r\n at the end is a carriage return and line feed, which is a common way to terminate a message in network communication. 
+//  7: This is the length of the message to be sent, in bytes. In this case, the length of the string "+PONG\r\n" is 7 bytes:
+//
+//+ (1 byte)
+//P (1 byte)
+//O (1 byte)
+//N (1 byte)
+//G (1 byte)
+//\r (1 byte)
+//\n (1 byte)
+//The send function will send exactly 7 bytes of data to the client.
+//
+//0: This is the flags argument to the send function. In this case, it's set to 0, which means no special flags are used. The send function will perform a normal,
+// blocking send operation.
+  
+  
+  std::cout << "Client connected\n";
   while (1) {
-    std::cout << "Waiting for a client to connect...\n";
-    auto client = accept(server_fd, (struct sockaddr *)&client_addr,
-                         (socklen_t *)&client_addr_len);
-    std::cout << "Client " << client << " connected\n";
-    
-    threads.push_back(
-        std::thread(handleConnection, std::ref(database), client));
+  char* msg[100];
+  if (recv(client_fd, (void*) msg, 100, 0) == -1) {
+    std::cerr << "Failed to receive message\n";
+    return 1;
   }
-  for (auto &t : threads) {
-    t.join();
+  const char* message = "+PONG\r\n";
+  if (send(client_fd, (const void*) message, strlen(message), 0) == -1) {
+    std::cerr << "Failed to send message\n";
+    return 1;
   }
-  close(server_fd);
+  }
+ 
+  close(server_fd);  return 0;
+  close(server_fd);  
   return 0;
 }
