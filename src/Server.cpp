903ad40fbac22fda2150aa1871165bba925c4e3f
MeThakur <menotthakur@gmail.com>
2024-08-30 09:03:36 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -9,71 +9,80 @@
 #include <netdb.h>
 #include <thread>
 #include <vector>
+#include <unordered_map>
 #include "./RedisParser.hpp"
-#define BUFFER_SIZE 128
+
+#define BUFFER_SIZE 1024
+
+std::unordered_map<std::string, std::string> data_store;
+
 void handle(int fd)
 { 
   char buff[BUFFER_SIZE] = "";
-  bzero(&buff,sizeof(buff));
   while(1)
   { 
-    memset(&buff,'\0',sizeof(buff));
-    recv(fd, buff, 15, 0);
-    if(strcasecmp(buff, "exit") == 0)
-  
+    memset(&buff, '\0', sizeof(buff));
     int recv_bytes = recv(fd, buff, sizeof(buff), 0);
-    if(recv_bytes <= 0 )
-      return ;
-    std::string recv_str(buff ,recv_bytes);
+    if(recv_bytes <= 0)
+      return;
+    
+    std::string recv_str(buff, recv_bytes);
     Redisparser request(recv_str);
-    std::vector<std::string> argStr =  request.parser();
-    // if(argStr.empty())
-    // {
-    //   send(fd, argStr[0].c_str() ,argStr[0].size(), 0);
-    //   return ;
-    // }
-    std::string reply = argStr[0];
-    if(reply == "ping")
+    std::vector<std::string> argStr = request.parser();
+    
+    if(argStr.empty())
+      continue;
+
+    std::string reply;
+    std::string command = argStr[0];
+    
+    if(command == "ping")
     {
-      send(fd, "+PONG\r\n",7, 0);
+      reply = "+PONG\r\n";
     }
-    else if( reply == "echo" )
+    else if(command == "echo" && argStr.size() > 1)
     { 
-      for(int i = 1; i < argStr.size() ; ++ i)
-        reply = "$"+ std::to_string(argStr[i].size()) + "\r\n" +argStr[i] + "\r\n";
-        send(fd, reply.c_str() , reply.size(), 0);
+      reply = "$" + std::to_string(argStr[1].size()) + "\r\n" + argStr[1] + "\r\n";
+    }
+    else if(command == "set" && argStr.size() >= 3)
+    {
+      data_store[argStr[1]] = argStr[2];
+      reply = "+OK\r\n";
+    }
+    else if(command == "get" && argStr.size() == 2)
+    {
+      auto it = data_store.find(argStr[1]);
+      if(it != data_store.end())
+      {
+        reply = "$" + std::to_string(it->second.size()) + "\r\n" + it->second + "\r\n";
+      }
+      else
+      {
+        reply = "$-1\r\n";
+      }
     }
     else
     {
-      break;
-    if(strcasecmp(buff,"*1\r\n$4\r\nping\r\n") !=0 )
-    {  
-      bzero(&buff,sizeof(buff));
-      continue;
+      reply = "-ERR unknown command\r\n";
     }
-    send(fd, "+PONG\r\n",7, 0);
+
+    send(fd, reply.c_str(), reply.size(), 0);
   }
-  return ;
 }
+
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  // std::cout << "Logs from your program will appear here!\n";
-  // Uncomment this block to pass the first stage
-  //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
    return 1;
   }
-  //
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
+
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  //
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -95,17 +104,20 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   std::vector<std::thread> cli_threads;
+  
   while(1)
   {
     int clientFd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
     std::cout << "Client connected\n";
-    cli_threads.push_back(std::thread(handle, clientFd) );
+    cli_threads.push_back(std::thread(handle, clientFd));
   }
+
   for(auto& cli_thread: cli_threads)
   {
     if(cli_thread.joinable())
       cli_thread.join();
   }
+
   close(server_fd);
   return 0;
 }
