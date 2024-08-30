e5c07622584cdab4aecbfe034afeac692d5ac78b
MeThakur <menotthakur@gmail.com>
2024-08-30 09:08:47 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,7 +7,6 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <thread>
 #include <vector>
 #include <unordered_map>
 #include <sstream>
@@ -24,93 +23,95 @@ std::vector<std::string> parse_resp(const std::string& input) {
 
     std::getline(iss, line);
     if (line[0] != '*') {
+        std::cerr << "Invalid format: expected '*', got '" << line << "'" << std::endl;
         return result;  // Invalid format
     }
 
     int array_length = std::stoi(line.substr(1));
+    std::cerr << "Array length: " << array_length << std::endl;
 
     for (int i = 0; i < array_length; ++i) {
         std::getline(iss, line);
         if (line[0] != '$') {
+            std::cerr << "Invalid format: expected '$', got '" << line << "'" << std::endl;
             return {};  // Invalid format
         }
 
         int str_length = std::stoi(line.substr(1));
         std::getline(iss, line);
         if (line.length() != str_length) {
+            std::cerr << "Invalid length: expected " << str_length << ", got " << line.length() << std::endl;
             return {};  // Invalid length
         }
 
         result.push_back(line);
+        std::cerr << "Parsed argument: " << line << std::endl;
     }
 
     return result;
 }
 
-void handle(int fd)
-{ 
-    char buff[BUFFER_SIZE] = "";
-    while(1)
-    { 
-        memset(&buff, '\0', sizeof(buff));
-        int recv_bytes = recv(fd, buff, sizeof(buff), 0);
-        if(recv_bytes <= 0)
-            return;
+void handle_client(int client_fd) {
+    char buff[BUFFER_SIZE];
+    while (true) {
+        memset(buff, 0, BUFFER_SIZE);
+        ssize_t bytes_received = recv(client_fd, buff, BUFFER_SIZE - 1, 0);
         
-        std::string recv_str(buff, recv_bytes);
-        std::vector<std::string> argStr = parse_resp(recv_str);
-        
-        if(argStr.empty())
+        if (bytes_received <= 0) {
+            std::cerr << "Client disconnected or error occurred" << std::endl;
+            break;
+        }
+
+        std::string received_data(buff, bytes_received);
+        std::cerr << "Received data: " << received_data << std::endl;
+
+        std::vector<std::string> args = parse_resp(received_data);
+        if (args.empty()) {
+            std::cerr << "Failed to parse RESP" << std::endl;
             continue;
+        }
 
-        std::string reply;
-        std::string command = argStr[0];
+        std::string command = args[0];
         std::transform(command.begin(), command.end(), command.begin(), ::tolower);
-        
-        if(command == "ping")
-        {
-            reply = "+PONG\r\n";
-        }
-        else if(command == "echo" && argStr.size() > 1)
-        { 
-            reply = "$" + std::to_string(argStr[1].size()) + "\r\n" + argStr[1] + "\r\n";
-        }
-        else if(command == "set" && argStr.size() >= 3)
-        {
-            data_store[argStr[1]] = argStr[2];
-            reply = "+OK\r\n";
-        }
-        else if(command == "get" && argStr.size() == 2)
-        {
-            auto it = data_store.find(argStr[1]);
-            if(it != data_store.end())
-            {
-                reply = "$" + std::to_string(it->second.size()) + "\r\n" + it->second + "\r\n";
+
+        std::string response;
+        if (command == "ping") {
+            response = "+PONG\r\n";
+        } else if (command == "echo" && args.size() > 1) {
+            response = "$" + std::to_string(args[1].length()) + "\r\n" + args[1] + "\r\n";
+        } else if (command == "set" && args.size() >= 3) {
+            data_store[args[1]] = args[2];
+            response = "+OK\r\n";
+        } else if (command == "get" && args.size() == 2) {
+            auto it = data_store.find(args[1]);
+            if (it != data_store.end()) {
+                response = "$" + std::to_string(it->second.length()) + "\r\n" + it->second + "\r\n";
+            } else {
+                response = "$-1\r\n";
             }
-            else
-            {
-                reply = "$-1\r\n";
-            }
-        }
-        else
-        {
-            reply = "-ERR unknown command '" + command + "'\r\n";
+        } else {
+            response = "-ERR unknown command '" + command + "'\r\n";
         }
 
-        send(fd, reply.c_str(), reply.size(), 0);
+        std::cerr << "Sending response: " << response << std::endl;
+        ssize_t bytes_sent = send(client_fd, response.c_str(), response.length(), 0);
+        if (bytes_sent <= 0) {
+            std::cerr << "Failed to send response" << std::endl;
+            break;
+        }
     }
 }
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        std::cerr << "Failed to create server socket\n";
+        std::cerr << "Failed to create server socket" << std::endl;
         return 1;
     }
 
     int reuse = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        std::cerr << "setsockopt failed\n";
+        std::cerr << "setsockopt failed" << std::endl;
         return 1;
     }
 
@@ -118,32 +119,32 @@ int main() {
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(6379);
-    
-    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-        std::cerr << "Failed to bind to port 6379\n";
+
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+        std::cerr << "Failed to bind to port 6379" << std::endl;
         return 1;
     }
-    
-    int connection_backlog = 5;
-    if (listen(server_fd, connection_backlog) != 0) {
-        std::cerr << "listen failed\n";
+
+    if (listen(server_fd, 5) != 0) {
+        std::cerr << "listen failed" << std::endl;
         return 1;
     }
-    
-    std::cout << "Waiting for a client to connect...\n";
-    
-    while(1)
-    {
+
+    std::cout << "Server listening on port 6379" << std::endl;
+
+    while (true) {
         struct sockaddr_in client_addr;
-        int client_addr_len = sizeof(client_addr);
-        int clientFd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-        if (clientFd < 0) {
-            std::cerr << "Failed to accept client connection\n";
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+        
+        if (client_fd < 0) {
+            std::cerr << "Failed to accept client connection" << std::endl;
             continue;
         }
-        std::cout << "Client connected\n";
-        handle(clientFd);
-        close(clientFd);
+
+        std::cout << "New client connected" << std::endl;
+        handle_client(client_fd);
+        close(client_fd);
     }
 
     close(server_fd);
