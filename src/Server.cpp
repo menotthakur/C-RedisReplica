6685225b80cbb72a468582c56072b9243e51169d
MeThakur <menotthakur@gmail.com>
2024-08-31 03:55:07 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,150 +2,113 @@
 #include <cstdlib>
 #include <string>
 #include <cstring>
+#include <thread>
+#include <vector>
+#include <string>
+#include <unordered_map>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <thread>
-#include <vector>
-#include <pthread.h>
-#include <cassert>
-#include <map>
-const int BUFFER_SIZE = 2048;
-int head = 0;
-std::string data;
-std::map<std::string, std::string> storage;
-std::map<std::string, std::chrono::time_point<std::chrono::system_clock>> expiry;
-void err(std::string msg) {
-  std::cerr << "ERROR: " << msg << std::endl;
-}
-char nextToken() {
-  if (head == data.size()) return '\0';
-  return data[head++];
-}
-void match(char c) {
-  char token = nextToken();
-  if (c != token) err("Expected " + std::string(1, c) + " , found " + std::string(1, token));
-}
-std::string parseBulkString() {
-  int len = 0;
-  while (1) {
-    char c = nextToken();
-    if (c >= '0' && c <= '9') {
-      len = len * 10 + c - '0';
-    } else break;
+#define BUFFER_SIZE 1024
+#define MAX_CONNECTIONS 30
+std::vector<std::string> splitRedisCommand(std::string input, std::string separator, int separatorLength) {
+  std::vector<std::string> res;
+  std::size_t foundSeparator = input.find(separator);
+  if (foundSeparator == std::string::npos) {
+    res.push_back(input);
   }
-  match('\n');
-  std::string msg;
-  for (int i = 0; i < len; ++i) {
-    msg += nextToken();
+  while (foundSeparator != std::string::npos) {
+    std::string splitOccurrence = input.substr(0, foundSeparator);
+    res.push_back(splitOccurrence);
+    input = input.substr(foundSeparator + separatorLength);
+    foundSeparator = input.find(separator);
   }
-  match('\r');
-  match('\n');
-  return msg;
+  return res;
 }
-std::vector<std::string> parseCommand() {
-  std::vector<std::string> cmds;
-  head = 0;
-  match('*');
-  int argc = 0;
-  while (1) {
-    char c = nextToken();
-    if (c >= '0' && c <= '9') {
-      argc = argc * 10 + c - '0';
-    } else break;
-  }
-  match('\n');
-  for (int i = 0; i < argc; ++i) {
-    match('$');
-    cmds.push_back(parseBulkString());  
-  }
-  return cmds;
-}
-void handleRequest(int client_fd) {
-  char buffer[BUFFER_SIZE] = "";
-  std::cout << "Handling request from client " << client_fd << "\n";
-  while (true) {
-    while (recv(client_fd, buffer, BUFFER_SIZE, 0)) {
-      std::string request(buffer), response;
-      data = request;
-      std::vector<std::string> cmds = parseCommand();
-      if (cmds[0] == "ping") {
-        response = "+PONG\r\n";
-      } else if (cmds[0] == "echo") {
-        std::string argv = cmds[1];
-        response = "$" + std::to_string(argv.size()) + "\r\n" + argv + "\r\n";      
-      } else if (cmds[0] == "get") {
-        std::string key = cmds[1];
-        //if (storage.find(key) == storage.end()) {
-        if (storage.find(key) == storage.end() || expiry[key] < std::chrono::system_clock::now()) {
-          response = "$-1\r\n";
-        } else {
-          std::string value = storage[key];
-          response = "$" + std::to_string(value.size()) + "\r\n" + value + "\r\n";
-        }    
-      } else if (cmds[0] == "set") {
-        std::chrono::time_point<std::chrono::system_clock> exp;
-        std::string key = cmds[1];
-        std::string value = cmds[2];
-        if (cmds.size() == 5) {
-          int ttl = std::stoi(cmds[4]);
-          exp = std::chrono::system_clock::now() + std::chrono::milliseconds(ttl);
-        } else {
-          exp = std::chrono::system_clock::now() + std::chrono::seconds(1000);
-        }
-        storage[key] = value;
-        expiry[key] = exp;
-        response = "$2\r\nOK\r\n";
+std::unordered_map<std::string, std::string> dictionary = {};
+void handle_connection(int client) {
+  std::cout << "Client connected" << client << "\n";
+  char buffer[BUFFER_SIZE];
+  int bytes_read;
+  const char pong_response = "+PONG\r\n";
+  while ((bytes_read = read(client, buffer, BUFFER_SIZE-1))>0){
+    buffer[bytes_read] = '\0'; //null terminate the buffer
+    std::cout << "received: " << buffer << std::endl;
+    std::string input(buffer, strlen(buffer));
+    std::vector<std::string> tokens = splitRedisCommand(input, "\r\n", 2);
+    std::string cmd = "";
+    for (const auto& x: tokens[2]){
+        cmd += tolower(x);
+    }
+    if (cmd == "ping"){
+        send(client, pong_response, strlen(pong_response), 0);
+    } else if (cmd == "echo") {
+        std::string echo_res = tokens[3] + "\r\n" + tokens[4] + "\r\n";
+        send(client, echo_res.data(), echo_res.length(), 0);
+    } else if (cmd == "set"){
+        dictionary[tokens[4]] = tokens[6];
+        send(client, "+OK\r\n", 5, 0);
+    } else if (cmd == "get"){
+      if (dictionary.find(tokens[4]) == dictionary.end()){
+        send(client, "$-1\r\n", 5, 0);
+      } else {
+        std::string g_response = "$" + std::to_string(dictionary[tokens[4]].size()) + "\r\n" + dictionary[tokens[4]] + "\r\n";
+        send(client, g_response.data(), g_response.length(), 0);
       }
-      send(client_fd, response.c_str(), response.size(), 0);
     }
   }
+  close(client);
 }
 int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+  // std::cout << "Logs from your program will appear here!\n";
+  // Uncomment this block to pass the first stage
+  //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
+   std::cerr << "Failed to create server socket\n";
+   return 1;
   }
+  //
+  // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
+  //
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
+  //
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
+  //
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+  //
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
+
   std::cout << "Waiting for a client to connect...\n";
-  while (true) {
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    if (client_fd == -1) return 1;
-    std::cout << "Client connected\n";
-    pid_t pid = fork();
-    if (pid == 0) {
-      handleRequest(client_fd);
-      close(client_fd);
-      exit(0);
-    } else {
-      close(client_fd);
-    }
-  }
+
+  int n_connections = 0;
+  do {
+    int client_fd;
+    client_fd = accept(server_fd,(struct sockaddr*) &client_addr, (socklen_t *)&client_addr_len);
+    std::thread t(handle_connection, client_fd);
+    t.detach();
+    ++n_connections;
+  } while (n_connections < MAX_CONNECTIONS);
   close(server_fd);
   return 0;
 }
